aaddd602254318943f88e7dae7ab849158cbb24c
Maria Grimaldi <maria.grimaldi@edunext.co>
2025-05-28 09:13:16 +0200

    docs: fix quality issues with module docstring

--- openedx_user_groups/__init__.py
+++ openedx_user_groups/__init__.py
@@ -1,5 +1,6 @@
 """
-Specifications for user group methods, data models, and APIs used in the management of user groups within the Open edX platform.
+Specifications for user group methods, data models, and APIs used in the management of user groups within
+the Open edX platform.
 """
 
 __version__ = '0.1.0'
